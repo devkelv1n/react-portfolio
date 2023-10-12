6d21c09dd31a857ce85ac66e8edd675b67460ee4
devkelv1n <devkelvin@hotmail.com>
2023-10-13 01:00:15 +0800

    Github page error
    If open github page with url will redirect to github page 404, added this code to prevent this happen and redirect to correct page.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
@@ -9,10 +9,11 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/react-portfolio/" element={<Layout />}>
         <Route index element={<Home/>} />
         <Route path="about" element={<About/>} />
         <Route path="contact" element={<Contact/>} />
+        <Route path='*' element={<Navigate replace to='/404'/>} />
       </Route>
     </Routes>
     </>
